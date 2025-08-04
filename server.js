b5fe87abd357b0d56bf2293a588106365c613925
Eyob Hailu <EyobHailu246@gmail.com>
2025-08-04 22:07:41 +0300

    database integrated

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
-const app = require('./app')
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
 
-console.log(app.get('env'))
+dotenv.config({ path: './config.env' });
 
-const port = 3000;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  // .connect(process.env.DATABASE_LOCAL,{
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful'));
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
