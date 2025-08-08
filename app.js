77efdcdeacd50d4cea7dc4993ea5279f3514f901
Eyob Hailu <EyobHailu246@gmail.com>
2025-08-08 22:07:35 +0300

    error handling

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError')
+const globalErrorhandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -8,19 +10,12 @@ const app = express();
 
 // 1, MIDDLEWARE
 
-
-if(process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-
 app.use(express.json());
 app.use(express.static('./public'));
-
-app.use((req, res, next) => {
-
-  next();
-});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -28,8 +23,18 @@ app.use((req, res, next) => {
 });
 
 //3. ROUTES
-app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-//4. START A SERVER
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `can't find ${req.originalUrl} on this server`,
+  // });
+
+  next(new AppError(`can't find ${req.originalUrl} on this server`,404));
+});
+
+app.use(globalErrorhandler);
+
 module.exports = app;
