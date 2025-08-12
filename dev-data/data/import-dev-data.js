17533ddeaec52f0e9c8d9f90482bb3f5cef1c14e
Eyob Hailu <EyobHailu246@gmail.com>
2025-08-12 17:23:49 +0300

    review commit

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -20,7 +20,7 @@ mongoose
   .then(() => console.log('DB connection successful'));
 
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+  fs.readFileSync(`${__dirname}/tours.json`, 'utf-8')
 );
 
 //IMPORT DATA IN DB
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const User = require('./userModel');
 // const validator = require('validator')
 
 const tourSchema = new mongoose.Schema(
@@ -47,8 +48,8 @@ const tourSchema = new mongoose.Schema(
     priceDiscount: {
       type: Number,
       validate: {
-        validator:function(val) {
-         return val < this.price;
+        validator: function (val) {
+          return val < this.price;
         },
         message: 'discount price should be less than regular price',
       },
@@ -77,6 +78,36 @@ const tourSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    startLocation: {
+      //Geo JSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "User"
+      }
+    ],
   },
   {
     toJSON: { virtuals: true },
@@ -94,6 +125,12 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
+// tourSchema.pre('save', async function (next) {
+//   const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
+
 //QUERY MIDDLEWARE
 tourSchema.pre(/^find/, function (next) {
   // tourSchema.pre('find',function(next){
@@ -102,6 +139,16 @@ tourSchema.pre(/^find/, function (next) {
   this.start = Date.now();
   next();
 });
+
+tourSchema.pre(/^find/,function(next){
+  this.populate({
+    path:'guides',
+    select:'-__v -passwordChangedAt'
+  });
+  next();
+})
+
+
 tourSchema.post(/^find/, function (docs, next) {
   // tourSchema.pre('find',function(next){
   console.log(`Query took ${Date.now() - this.start} milliseconds`);
